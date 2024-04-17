ce09bc3c9d2693eff807563a674adb3554bdbeb8
Ioshua Gabriel Ochoa Martínez <159966658+ioshua8aM@users.noreply.github.com>
2024-04-17 00:22:37 +0000

    Versión 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
-#inlcude <iostream>
+#include <iostream>
+#include <string>
+#include <thread>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+
+using namespace std;
+using namespace ftxui;
+
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Space invaders" >> std::endl:
+    Element lienzo = hbox({
+        spinner(21,4) | bold
+    });
+
+    Screen pantalla = Screen::Create(
+        Dimension::Full(),
+        Dimension::Fit(lienzo)
+        );
+
+    Render(pantalla,lienzo);
+    pantalla.Print();
+    pantalla.ResetPosition();
+    this_thread::sleep_for(0.1s);
+
     return 0;
 }
