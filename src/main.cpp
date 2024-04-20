3a2cd19f0144e217999648c258dd6f46fc8919f0
Ioshua Gabriel Ochoa Martínez <159966658+ioshua8aM@users.noreply.github.com>
2024-04-20 20:01:58 +0000

    Versión 4

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,16 @@ using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
+   
+   
+    int fotograma = 0;
+
+    while(true)
+    {
+        fotograma++;
+    Element personaje = spinner(21,fotograma) | bold | color(Color::Yellow1) | bgcolor(Color::Green1);
     Element lienzo = hbox({
-        spinner(21,4) | bold
+        spinner(21,fotograma) | bold
     });
 
     Screen pantalla = Screen::Create(
@@ -21,8 +29,8 @@ int main(int argc, char const *argv[])
 
     Render(pantalla,lienzo);
     pantalla.Print();
-    pantalla.ResetPosition();
+    cout << pantalla.ResetPosition();
     this_thread::sleep_for(0.1s);
-
+    }
     return 0;
 }
