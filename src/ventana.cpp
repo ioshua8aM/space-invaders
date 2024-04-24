dd58a4fb043d4fcbb8c435ee2806fb1569c11c3a
Ioshua Gabriel Ochoa Martínez <159966658+ioshua8aM@users.noreply.github.com>
2024-04-24 00:23:36 +0000

    Versión 6.1

--- src/ventana.cpp
+++ src/ventana.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 #include <ftxui/screen/screen.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <thread>
 
 using namespace std;
 using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    auto pantalla = Screen::Create(Dimension::Full(),Dimension::Fixed(10));
-    Render(pantalla,border(text ("hola")));
-    pantalla.PixelAt(5,5).character = 's';
-    pantalla.Print();
-
-    string prueba = ("Hola mundo");
+    string prueba = ("DVD");
     int posicionX = 0;
     int posicionY = 0;
-
-    for(auto &&letra : prueba)
+    while(true)
     {
-        pantalla.PixelAt(posicionX, posicionY).character = letra;
-        posicionX++;
+        this_thread::sleep_for(0.1s);
+        auto pantalla = Screen::Create(Dimension::Full(), Dimension::Fixed(10));
+        Render(pantalla, border(text("hola")));
+        pantalla.PixelAt(5, 5).character = 's';
+        pantalla.Print();
+
+        
+
+        for (auto &&letra : prueba)
+        {
+            pantalla.PixelAt(posicionX, posicionY).character = letra;
+            posicionX++;
+        }
+        pantalla.Print();
+        cout << pantalla.ResetPosition();
     }
-    pantalla.Print();
     return 0;
 }
